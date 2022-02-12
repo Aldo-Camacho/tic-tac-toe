8d30898cfa600a2693505033293e2e6044a9a091
Aldo Garcia <aldo.garcia@fundacion-jala.org>
2022-02-12 13:52:49 -0600

    UI restyled. Bug fixed (clicking a set cell ignored your turn). Restart button implemented. Player scores implemented.

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,8 +1,9 @@
 let turn = true;
-let players = [', '];
-const player1Class = 'black';
-const player2Class = 'red';
-const emptyTileClass = 'white'
+let players = ['', 'Computer'];
+let scores = [0,0];
+const player1Class = 'player1';
+const player2Class = 'player2';
+const emptyTileClass = 'empty'
 
 document.addEventListener('DOMContentLoaded', function () {
     initialize();
@@ -24,15 +25,19 @@ function activateTiles() {
     const tiles = document.querySelectorAll('.tile');
     tiles.forEach(tile => {
         tile.onclick = () => {
-            if (turn) {
-                tile.classList.replace(`${emptyTileClass}`,`${player1Class}`);
-            } else {
-                tile.classList.replace(`${emptyTileClass}`,`${player2Class}`);
+            if (tile.classList.contains(emptyTileClass)) { 
+                if (turn) {
+                    tile.classList.replace(emptyTileClass, player1Class);    
+                } else {
+                    tile.classList.replace(emptyTileClass, player2Class);
+                }
+                toggleTurn();
+                checkGame();
             }
-            toggleTurn();
-            checkGame();
         }
     });
+    
+    setScores();
 }
 
 function blockTiles() {
@@ -41,35 +46,44 @@ function blockTiles() {
         tile.onclick = () => {
         }
     });
+    setScores();
+}
+
+function restartPlayerNames(){
+    scores = [0,0];
+    for (let i = 1; i <3; i++){
+        const playerName = document.querySelector(`.player${i}Name`);
+        playerName.innerHTML = '';
+    }
 }
 
 function restartBoard() {
+    const random = Math.random();
+    if (random < 0.5) {
+        turn = true;
+    } else {
+        turn = false;
+    }
+    setPlayerHighlights();
     const output = document.querySelector('.winnerName');
-    for (let i = 1; i <3; i++){
-        const playerName = document.querySelector(`.player${i}Name`);
-        while (playerName.firstChild){
-            playerName.removeChild(playerName.lastChild);    
-        }
-    }
-    
-    while (output.firstChild){
-        output.removeChild(output.lastChild);
-    }
-    turn = true;
+    output.innerHTML = '';
+
     const tiles = document.querySelectorAll('.tile');
     tiles.forEach(tile => {
-        if (tile.classList.contains(`${player1Class}`)){
-            tile.classList.replace(`${player1Class}`,`${emptyTileClass}`);
-        } else if (tile.classList.contains(`${player2Class}`)){
-            tile.classList.replace(`${player2Class}`,`${emptyTileClass}`);
+        if (tile.classList.contains(player1Class)){
+            tile.classList.replace(player1Class, emptyTileClass);
+        } else if (tile.classList.contains(player2Class)){
+            tile.classList.replace(player2Class, emptyTileClass);
         }
     });
     activateTiles();
 }
 
+
 function singlePlayerButton() {
+    restartPlayerNames();
     const nameInput = document.querySelector('.nameInput');
-    nameInput.innerHTML = "";
+    nameInput.innerHTML = '';
     playerNameIn(1);
     restartBoard();
 }
@@ -83,7 +97,6 @@ function playerNameIn(i){
     nameInput.appendChild(textField);
     
     textField.onchange = () => {
-        console.log('Player name has changed')
         players[i-1] = textField.value;
         const label = document.createElement('p');
         label.textContent = `Player name = ${players[i-1]}`;
@@ -92,11 +105,11 @@ function playerNameIn(i){
         }
         nameSection.append(label);
         textField.remove();
-        
     }
 }
 
 function doublePlayerButton() {
+    restartPlayerNames();
     const nameInput = document.querySelector('.nameInput');
     nameInput.innerHTML ='';
 
@@ -108,6 +121,32 @@ function doublePlayerButton() {
 
 function toggleTurn(){
     turn = ! turn;
+    setPlayerHighlights();
+}
+
+function setPlayerHighlights() {
+    const nameContainers = document.querySelectorAll(".playerNameContainer");
+    if (turn) {
+        if (nameContainers[1].classList.contains('whiteNeon')){
+            nameContainers[1].classList.remove('whiteNeon');
+        }
+        nameContainers[0].classList.add('whiteNeon');
+    } else {
+        if (nameContainers[0].classList.contains('whiteNeon')){
+            nameContainers[0].classList.remove('whiteNeon');
+        }
+        nameContainers[1].classList.add('whiteNeon');
+    }
+}
+
+function setScores() {
+    for (let i = 1; i < 3; i++) {
+        const scoreCell = document.querySelector(`.player${i}Score`); 
+        scoreCell.innerHTML = '';
+        const label = document.createElement('p');
+        label.textContent = scores[i-1];
+        scoreCell.appendChild(label);
+    }
 }
 
 function checkGame(){
@@ -126,30 +165,40 @@ function checkGame(){
         [6, 7, 8]
     ];
 
+    let hasWinner = false;
     winScenarios.forEach(scenario => {
         if (allTiles[scenario[0]].classList.contains(player1Class) 
         && allTiles[scenario[1]].classList.contains(player1Class) 
-        && allTiles[scenario[2]].classList.contains(player1Class)){    
-        label.textContent = `${players[0]} wins`;
+        && allTiles[scenario[2]].classList.contains(player1Class)){ 
+            ++scores[0];
+            hasWinner = true;
+            label.textContent = `${players[0]} wins`;
             output.appendChild(label);
             blockTiles();
         } else if (allTiles[scenario[0]].classList.contains(player2Class) 
         && allTiles[scenario[1]].classList.contains(player2Class) 
         && allTiles[scenario[2]].classList.contains(player2Class)) {
+            ++scores[1];
+            hasWinner = true;
             label.textContent = `${players[1]} wins`;
             output.appendChild(label);
             blockTiles();
         }
     });
 
-    let flag = allTiles.forEach(tile => {
-        if (!tile.classList.contains(player1Class) && !tile.classList.contains(player1Class)) {
-             return false;
+    if (!hasWinner) {
+        let flag = true
+        allTiles.forEach(tile => {
+            if (!(tile.classList.contains(player1Class) || tile.classList.contains(player2Class))) {
+                flag = false;
+                return;
+            }
+        });
+        
+        if (flag) {
+            label.textContent = `Tie`;
+            output.appendChild(label);
+            blockTiles();
         }
-    });
-    if (flag) {
-        label.textContent = `Tie`;
-        output.appendChild(label);
-        blockTiles();
     }
 }
